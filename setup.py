cc716291d4ebe13956dfa667c828b892b59f596d
Michel Llorens <mllorens@dcc.uchile.cl>
2019-04-07 16:53:25 -0400

    setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='docketter',
+    version='1.0.0',
+    packages=[],
+    url='https://github.com/Michotastico/docketter',
+    license='MIT',
+    author='Michel Llorens',
+    author_email='mllorens@protonmail.com',
+    description='Python library and CLI to manage multiple docker-compose files.',
+    install_requires=[],
+)
